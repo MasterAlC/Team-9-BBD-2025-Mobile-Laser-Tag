d99d3b5b49ff812f85a5e54a477eb2517434aa6b
siyabonga-Mashilwane <siyabongamashilwane30@gmail.com>
2025-07-01 16:09:39 +0200

    Added new fields in terms of what defines a player

--- server/Player.js
+++ server/Player.js
@@ -3,10 +3,28 @@ class Player {
         this.id = id;
         this.socket = socket;
         this.score = 0;
+        this.host = false; // Indicates if this player is the host
+        this.spectator = false; // Indicates if this player is a spectator
     }
 
     updateScore(points) {
         this.score += points;
+    }
+
+    setHost(isHost) {
+        this.host = isHost;
+    }
+    setSpectator(isSpectator) {
+        this.spectator = isSpectator;
+    }
+    checkContradiction(){
+        if (this.host && this.spectator) {
+            this.socket.send(JSON.stringify({
+                type: 'error',
+                message: 'A player cannot be both a host and a spectator at the same time.'
+            }));
+            throw new Error("A player cannot be both a host and a spectator at the same time.");
+        }
     }
 }
 
