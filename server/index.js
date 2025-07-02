79277f6d14c2a3fc64a751ab9d3f5f8b3ecc534d
Alan Clark <alanc.za@icloud.com>
2025-07-02 21:33:23 +0200

    Change HTTP server creation based on PORT number for deployment with HTTP

--- server/index.js
+++ server/index.js
@@ -4,13 +4,9 @@ const path = require("path");
 const os = require("os");
 const fs = require("fs");
 const https = require("https");
+const http = require("http");
 const networkInterfaces = os.networkInterfaces();
 
-// SSL Certificate
-const options = {
-  key: fs.readFileSync(path.join(__dirname, "../cert", "server.key")),
-  cert: fs.readFileSync(path.join(__dirname, "../cert", "server.cert")),
-};
 
 // Setup express application
 const app = express();
@@ -27,9 +23,32 @@ const getLocalExternalIP = () => {
   return "localhost";
 };
 
+// Create HTTPS server
+const PORT = process.env.PORT || 3000;
+
+let server;
+if (PORT == 3000) {
+  // Use HTTPS with self-signed certificate for local development
+  const options = {
+    key: fs.readFileSync(path.join(__dirname, "../cert", "server.key")),
+    cert: fs.readFileSync(path.join(__dirname, "../cert", "server.cert")),
+  };
+  server = https.createServer(options, app);
+}
+else {
+  // Use HTTP for production or non-local environments
+  console.log("Running in production mode, using HTTP");
+  server = http.createServer(app);
+}
+
+// Start server
+const LOCAL_IP = getLocalExternalIP();
+server.listen(PORT, () => {
+  console.log(`Server running at: https://${LOCAL_IP}:${PORT}`);
+});
+
 const Game = require("./Game");
 const activeGames = new Map();
-
 function createGame(gameId) {
   if (!activeGames.get(gameId)) {
     activeGames.set(gameId, new Game(gameId));
@@ -67,16 +86,6 @@ module.exports = {
   startGame,
   playerHitEventHandler,
 };
-
-// Create HTTPS server
-const PORT = process.env.PORT || 3000;
-const server = https.createServer(options, app);
-
-// Start server
-const LOCAL_IP = getLocalExternalIP();
-server.listen(PORT, () => {
-  console.log(`Server running at: https://${LOCAL_IP}:${PORT}`);
-});
 
 // Web socket stuff to send and receive
 const { WebSocketServer } = require("ws");
